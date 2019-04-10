758daf8b27618db850e959a364d8ea47660f30a7
Arthur Douillard <arthur.douillard@heuritech.com>
2019-04-10 21:06:33 +0200

    [icarl] Fix several things.

--- inclearn/models/icarl.py
+++ inclearn/models/icarl.py
@@ -4,9 +4,8 @@ from torch import nn
 from torch.nn import functional as F
 from tqdm import trange
 
+from inclearn import factory, utils
 from inclearn.models.base import IncrementalLearner
-from inclearn import factory
-from inclearn import utils
 
 
 class ICarl(IncrementalLearner):
@@ -17,17 +16,22 @@ class ICarl(IncrementalLearner):
     def __init__(self, args):
         super().__init__()
 
-        self._device = args.device
-        self._memory_size = args.memory_size
-        self._opt_name = args.optimizer
-        self._lr = args.lr
-        self._n_epochs = args.epochs
+        self._device = args["device"]
+        self._memory_size = args["memory_size"]
+        self._opt_name = args["optimizer"]
+        self._lr = args["lr"]
+        self._weight_decay = args["weight_decay"]
+        self._n_epochs = args["epochs"]
 
-        self._k = args.memory_size
-        self._n_classes = args.increment
+        self._scheduling = args["scheduling"]
+        self._lr_decay = args["lr_decay"]
 
-        self._features_extractor = factory.get_resnet(args.convnet)
+        self._k = args["memory_size"]
+        self._n_classes = args["increment"]
+
+        self._features_extractor = factory.get_resnet(args["convnet"])
         self._classifier = nn.Linear(2048, self._n_classes, bias=False)
+        self._dropout = torch.nn.Dropout(args["dropout"], False)
 
         self._examplars = {}
         self._means = None
@@ -39,6 +43,7 @@ class ICarl(IncrementalLearner):
 
     def forward(self, x):
         x = self._features_extractor(x)
+        x = self._dropout(x)
         x = self._classifier(x)
         return x
 
@@ -46,23 +51,42 @@ class ICarl(IncrementalLearner):
     # Public API
     # ----------
 
-    def _before_task(self, data_loader):
+    def _before_task(self, train_loader, val_loader):
         if self._task == 0:
             self._previous_preds = None
         else:
             print("Computing previous predictions...")
-            self._previous_preds = self._compute_predictions(data_loader)
+            self._previous_preds = self._compute_predictions(train_loader)
+            if val_loader:
+                self._previous_preds_val = self._compute_predictions(val_loader)
+
             self._add_n_classes(self._task_size)
 
-        self._optimizer = factory.get_optimizer(self.parameters(), self._opt_name, self._lr)
+        self._optimizer = factory.get_optimizer(
+            self.parameters(),
+            self._opt_name,
+            self._lr,
+            self._weight_decay
+        )
 
-    def _train_task(self, data_loader):
+        self._scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            self._optimizer,
+            self._scheduling,
+            gamma=self._lr_decay
+        )
+
+    def _train_task(self, train_loader, val_loader):
+        print("nb ", len(train_loader.dataset))
+
         prog_bar = trange(self._n_epochs, desc="Losses.")
 
+        val_loss = 0.
         for epoch in prog_bar:
             _clf_loss, _distil_loss = 0., 0.
 
-            for idx, (idxes, inputs, targets) in enumerate(data_loader, start=1):
+            self._scheduler.step()
+
+            for idx, (idxes, inputs, targets) in enumerate(train_loader, start=1):
                 self._optimizer.zero_grad()
 
                 inputs, targets = inputs.to(self._device), targets.to(self._device)
@@ -75,52 +99,92 @@ class ICarl(IncrementalLearner):
                     idxes[1],
                 )
 
-            if not utils._check_loss(clf_loss) or not utils._check_loss(distil_loss):
-                import pdb; pdb.set_trace()
+                if not utils._check_loss(clf_loss) or not utils._check_loss(distil_loss):
+                    import pdb; pdb.set_trace()
 
-            loss = clf_loss + distil_loss
+                loss = clf_loss + distil_loss
 
-            loss.backward()
-            self._optimizer.step()
+                loss.backward()
+                self._optimizer.step()
 
-            _clf_loss += clf_loss.item()
-            _distil_loss += distil_loss.item()
+                _clf_loss += clf_loss.item()
+                _distil_loss += distil_loss.item()
 
-            if idx % 10 == 0 or idx >= len(data_loader):
-                prog_bar.set_description(
-                "Clf loss: {}; Distill loss: {}".format(
-                    round(clf_loss.item(), 3),
-                    round(distil_loss.item(), 3),
-                    )
-                )
+                if idx % 10 == 0 or idx >= len(train_loader):
+                    prog_bar.set_description(
+                    "Clf loss: {}; Distill loss: {}; Val loss: {}".format(
+                        round(clf_loss.item(), 3),
+                        round(distil_loss.item(), 3),
+                        round(val_loss, 3)
+                    ))
+
+            if val_loader is not None:
+                val_loss = self._compute_val_loss(val_loader)
+            prog_bar.set_description(
+            "Clf loss: {}; Distill loss: {}; Val loss: {}".format(
+                round(clf_loss.item(), 3),
+                round(distil_loss.item(), 3),
+                round(val_loss, 2)
+            ))
+
+
 
     def _after_task(self, data_loader):
         self._build_examplars(data_loader)
 
     def _eval_task(self, data_loader):
-        return self._classify(data_loader)
+        ypred, ytrue = self._classify(data_loader)
+        assert ypred.shape == ytrue.shape
+
+        return ypred, ytrue
+
 
     # -----------
     # Private API
     # -----------
 
-    def _compute_loss(self, logits, targets, idxes):
+    def _compute_val_loss(self, val_loader):
+        total_loss = 0.
+        c = 0
+
+        for idx, (idxes, inputs, targets) in enumerate(val_loader, start=1):
+            self._optimizer.zero_grad()
+
+            c += len(idxes)
+
+            inputs, targets = inputs.to(self._device), targets.to(self._device)
+            targets = utils.to_onehot(targets, self._n_classes).to(self._device)
+            logits = self.forward(inputs)
+
+            clf_loss, distil_loss = self._compute_loss(
+                logits,
+                targets,
+                idxes[1],
+                train=False
+            )
+
+            if not utils._check_loss(clf_loss) or not utils._check_loss(distil_loss):
+                import pdb; pdb.set_trace()
+
+            total_loss += (clf_loss + distil_loss).item()
+
+        return total_loss
+
+    def _compute_loss(self, logits, targets, idxes, train=True):
         if self._task == 0:
             # First task, only doing classification loss
             clf_loss = self._clf_loss(logits, targets)
             distil_loss = torch.zeros(1, device=self._device)
         else:
-            try:
-                clf_loss = self._clf_loss(
-                    logits[..., self._new_task_index:],
-                    targets[..., self._new_task_index:]
-                )
-            except:
-                import pdb; pdb.set_trace()
+            clf_loss = self._clf_loss(
+                logits[..., self._new_task_index:],
+                targets[..., self._new_task_index:]
+            )
 
+            previous_preds = self._previous_preds if train else self._previous_preds_val
             distil_loss = self._distil_loss(
                 logits[..., :self._new_task_index],
-                self._previous_preds[idxes, :self._new_task_index]
+                previous_preds[idxes, :self._new_task_index]
             )
 
         return clf_loss, distil_loss
@@ -159,7 +223,7 @@ class ICarl(IncrementalLearner):
             ypred.extend(preds)
             ytrue.extend(targets)
 
-        return ypred, ytrue
+        return np.array(ypred), np.array(ytrue)
 
     @property
     def _m(self):
