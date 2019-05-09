2b3965cb739e53a554775435c2f57fe7e6ec1398
Arthur Douillard <arthur.douillard@heuritech.com>
2019-05-09 22:22:56 +0200

    [common] Free more memory (still some leak) when running several consecutive exp.

--- inclearn/train.py
+++ inclearn/train.py
@@ -9,8 +9,11 @@ from inclearn import factory, results_utils, utils
 
 def train(args):
     seed_list = copy.deepcopy(args["seed"])
+    device = copy.deepcopy(args["device"])
+
     for seed in seed_list:
         args["seed"] = seed
+        args["device"] = device
         _train(args)
 
 
@@ -62,6 +65,11 @@ def _train(args):
 
     if args["name"]:
         results_utils.save_results(results, args["name"])
+
+    del model
+    del train_set
+    del test_set
+    torch.cuda.empty_cache()
 
 
 def _set_seed(seed):
