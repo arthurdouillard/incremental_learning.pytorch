acb0e65e38f840d409a009609caa7af24805496f
Arthur Douillard <arthur.douillard@heuritech.com>
2019-07-19 14:44:55 +0200

    [inclearn] Update meta-modules of the package.

--- inclearn/__main__.py
+++ inclearn/__main__.py
@@ -1,11 +1,17 @@
 from inclearn import parser
 from inclearn.train import train
 
-args = parser.get_parser().parse_args()
-args = vars(args)  # Converting argparse Namespace to a dict.
 
-if args["seed_range"] is not None:
-    args["seed"] = list(range(args["seed_range"][0], args["seed_range"][1] + 1))
-    print("Seed range", args["seed"])
+def main():
+    args = parser.get_parser().parse_args()
+    args = vars(args)  # Converting argparse Namespace to a dict.
 
-train(args)
+    if args["seed_range"] is not None:
+        args["seed"] = list(range(args["seed_range"][0], args["seed_range"][1] + 1))
+        print("Seed range", args["seed"])
+
+    train(args)
+
+
+if __name__ == "__main__":
+    main()
--- inclearn/models/__init__.py
+++ inclearn/models/__init__.py
@@ -1,7 +1,11 @@
-#from .e2e import End2End
 #from .fixedrepresentation import FixedRepresentation
 #from .focusforget import FocusForget
+from .bic import BiC
+from .e2e import End2End
 from .icarl import ICarl
+from .ucir import UCIR
+
+#from .icarl_mixup import ICarlMixUp
 
 #from .lwf import LwF
 #from .medic import Medic
