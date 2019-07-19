793360be9e05a69fb41c5461a64943978075e8c1
Arthur Douillard <arthur.douillard@heuritech.com>
2019-07-19 14:40:43 +0200

    [icarl] Decorrelate more herding from actual iCaRL algo.

--- inclearn/models/icarl.py
+++ inclearn/models/icarl.py
@@ -1,10 +1,12 @@
+import pdb
+
 import numpy as np
 import torch
 from scipy.spatial.distance import cdist
 from torch.nn import functional as F
 from tqdm import tqdm
 
-from inclearn.lib import factory, network, utils
+from inclearn.lib import factory, herding, network, utils
 from inclearn.models.base import IncrementalLearner
 
 EPSILON = 1e-8
@@ -34,9 +36,16 @@ class ICarl(IncrementalLearner):
         self._lr_decay = args["lr_decay"]
 
         self._memory_size = args["memory_size"]
+        self._fixed_memory = args["fixed_memory"]
         self._n_classes = 0
 
-        self._network = network.BasicNet(args["convnet"], device=self._device, use_bias=True)
+        self._network = network.BasicNet(
+            args["convnet"],
+            device=self._device,
+            use_bias=True,
+            extract_no_act=True,
+            classifier_no_act=False
+        )
 
         self._examplars = {}
         self._means = None
@@ -46,7 +55,7 @@ class ICarl(IncrementalLearner):
         self._clf_loss = F.binary_cross_entropy_with_logits
         self._distil_loss = F.binary_cross_entropy_with_logits
 
-        self._herding_matrix = []
+        self._herding_indexes = []
 
     def eval(self):
         self._network.eval()
@@ -80,39 +89,38 @@ class ICarl(IncrementalLearner):
             self._scheduler.step()
 
             prog_bar = tqdm(train_loader)
-            for i, (inputs, targets) in enumerate(prog_bar, start=1):
+            for i, (inputs, targets, memory_flags) in enumerate(prog_bar, start=1):
                 self._optimizer.zero_grad()
 
-                loss = self._forward_loss(inputs, targets)
-
-                if not utils._check_loss(loss):
-                    import pdb
-                    pdb.set_trace()
-
+                loss = self._forward_loss(inputs, targets, memory_flags)
                 loss.backward()
+
                 self._optimizer.step()
 
                 _loss += loss.item()
 
                 if val_loader is not None and i == len(train_loader):
-                    for inputs, targets in val_loader:
-                        val_loss += self._forward_loss(inputs, targets).item()
+                    for inputs, targets, memory_flags in val_loader:
+                        val_loss += self._forward_loss(inputs, targets, memory_flags).item()
 
                 prog_bar.set_description(
                     "Task {}/{}, Epoch {}/{} => Clf loss: {}, Val loss: {}".format(
-                        self._task + 1, self._n_tasks,
-                        epoch + 1, self._n_epochs,
-                        round(_loss / i, 3),
-                        round(val_loss, 3)
+                        self._task + 1, self._n_tasks, epoch + 1, self._n_epochs,
+                        round(_loss / i, 3), round(val_loss, 3)
                     )
                 )
 
-    def _forward_loss(self, inputs, targets):
+    def _forward_loss(self, inputs, targets, memory_flags):
         inputs, targets = inputs.to(self._device), targets.to(self._device)
-        targets = utils.to_onehot(targets, self._n_classes).to(self._device)
+        onehot_targets = utils.to_onehot(targets, self._n_classes).to(self._device)
         logits = self._network(inputs)
 
-        return self._compute_loss(inputs, logits, targets)
+        loss = self._compute_loss(inputs, logits, targets, onehot_targets, memory_flags)
+
+        if not utils._check_loss(loss):
+            pdb.set_trace()
+
+        return loss
 
     def _after_task(self, inc_dataset):
         self.build_examplars(inc_dataset)
@@ -120,7 +128,7 @@ class ICarl(IncrementalLearner):
         self._old_model = self._network.copy().freeze()
 
     def _eval_task(self, data_loader):
-        ypred, ytrue = compute_accuracy(self._network, data_loader, self._class_means)
+        ypred, ytrue = self.compute_accuracy(self._network, data_loader, self._class_means)
 
         return ypred, ytrue
 
@@ -128,13 +136,13 @@ class ICarl(IncrementalLearner):
     # Private API
     # -----------
 
-    def _compute_loss(self, inputs, logits, targets):
+    def _compute_loss(self, inputs, logits, targets, onehot_targets, memory_flags):
         if self._old_model is None:
-            loss = F.binary_cross_entropy_with_logits(logits, targets)
+            loss = F.binary_cross_entropy_with_logits(logits, onehot_targets)
         else:
             old_targets = torch.sigmoid(self._old_model(inputs).detach())
 
-            new_targets = targets.clone()
+            new_targets = onehot_targets.clone()
             new_targets[..., :-self._task_size] = old_targets
 
             loss = F.binary_cross_entropy_with_logits(logits, new_targets)
@@ -181,6 +189,8 @@ class ICarl(IncrementalLearner):
     @property
     def _memory_per_class(self):
         """Returns the number of examplars per class."""
+        if self._fixed_memory:
+            return self._memory_size // self._total_n_classes
         return self._memory_size // self._n_classes
 
     # -----------------
@@ -190,102 +200,75 @@ class ICarl(IncrementalLearner):
     def build_examplars(self, inc_dataset):
         print("Building & updating memory.")
 
-        self._data_memory, self._targets_memory = [], []
+        data_memory, targets_memory = [], []
         self._class_means = np.zeros((100, self._network.features_dim))
 
         for class_idx in range(self._n_classes):
+            # We extract the features, both normal and flipped:
             inputs, loader = inc_dataset.get_custom_loader(class_idx, mode="test")
-            features, targets = extract_features(
-                self._network, loader
-            )
-            features_flipped, _ = extract_features(
-                self._network, inc_dataset.get_custom_loader(class_idx, mode="flip")[1]
+            features, targets = utils.extract_features(self._network, loader)
+            features_flipped, _ = utils.extract_features(
+                self._network,
+                inc_dataset.get_custom_loader(class_idx, mode="flip")[1]
             )
 
             if class_idx >= self._n_classes - self._task_size:
-                self._herding_matrix.append(select_examplars(
-                    features, self._memory_per_class
-                ))
+                # New class, selecting the examplars:
+                memory = self.get_memory() if self._task > 0 else None
+                self._herding_indexes.append(
+                    #herding.minimize_confusion(
+                    #    inc_dataset, self._network, memory, class_idx, self._memory_per_class
+                    #)
+                    herding.icarl_selection(features, self._memory_per_class)
+                )
 
-            examplar_mean, alph = compute_examplar_mean(
-                features, features_flipped, self._herding_matrix[class_idx], self._memory_per_class
+            # Reducing examplars:
+            selected_indexes = self._herding_indexes[class_idx][:self._memory_per_class]
+            self._herding_indexes[class_idx] = selected_indexes
+
+            # Re-computing the examplar mean (which may have changed due to the training):
+            examplar_mean = self.compute_examplar_mean(
+                features, features_flipped, selected_indexes, self._memory_per_class
             )
-            self._data_memory.append(inputs[np.where(alph == 1)[0]])
-            self._targets_memory.append(targets[np.where(alph == 1)[0]])
+
+            data_memory.append(inputs[selected_indexes])
+            targets_memory.append(targets[selected_indexes])
 
             self._class_means[class_idx, :] = examplar_mean
 
-        self._data_memory = np.concatenate(self._data_memory)
-        self._targets_memory = np.concatenate(self._targets_memory)
+        self._data_memory = np.concatenate(data_memory)
+        self._targets_memory = np.concatenate(targets_memory)
 
     def get_memory(self):
         return self._data_memory, self._targets_memory
 
+    @staticmethod
+    def compute_examplar_mean(feat_norm, feat_flip, indexes, nb_max):
+        D = feat_norm.T
+        D = D / (np.linalg.norm(D, axis=0) + EPSILON)
 
-def extract_features(model, loader):
-    targets, features = [], []
+        D2 = feat_flip.T
+        D2 = D2 / (np.linalg.norm(D2, axis=0) + EPSILON)
 
-    for _inputs, _targets in loader:
-        _targets = _targets.numpy()
-        _features = model.extract(_inputs.to(model.device)).detach().cpu().numpy()
+        selected_d = D[..., indexes]
+        selected_d2 = D2[..., indexes]
 
-        features.append(_features)
-        targets.append(_targets)
+        mean = (np.mean(selected_d, axis=1) + np.mean(selected_d2, axis=1)) / 2
+        mean /= (np.linalg.norm(mean) + EPSILON)
 
-    return np.concatenate(features), np.concatenate(targets)
+        return mean
 
+    @staticmethod
+    def compute_accuracy(model, loader, class_means):
+        features, targets_ = utils.extract_features(model, loader)
 
-def select_examplars(features, nb_max):
-    D = features.T
-    D = D / (np.linalg.norm(D, axis=0) + EPSILON)
-    mu = np.mean(D, axis=1)
-    herding_matrix = np.zeros((features.shape[0],))
+        targets = np.zeros((targets_.shape[0], 100), np.float32)
+        targets[range(len(targets_)), targets_.astype('int32')] = 1.
 
-    w_t = mu
-    iter_herding, iter_herding_eff = 0, 0
+        features = (features.T / (np.linalg.norm(features.T, axis=0) + EPSILON)).T
 
-    while not (
-        np.sum(herding_matrix != 0) == min(nb_max, features.shape[0])
-    ) and iter_herding_eff < 1000:
-        tmp_t = np.dot(w_t, D)
-        ind_max = np.argmax(tmp_t)
-        iter_herding_eff += 1
-        if herding_matrix[ind_max] == 0:
-            herding_matrix[ind_max] = 1 + iter_herding
-            iter_herding += 1
+        # Compute score for iCaRL
+        sqd = cdist(class_means, features, 'sqeuclidean')
+        score_icarl = (-sqd).T
 
-        w_t = w_t + mu - D[:, ind_max]
-
-    return herding_matrix
-
-
-def compute_examplar_mean(feat_norm, feat_flip, herding_mat, nb_max):
-    D = feat_norm.T
-    D = D / (np.linalg.norm(D, axis=0) + EPSILON)
-
-    D2 = feat_flip.T
-    D2 = D2 / (np.linalg.norm(D2, axis=0) + EPSILON)
-
-    alph = herding_mat
-    alph = (alph > 0) * (alph < nb_max + 1) * 1.
-
-    alph_mean = alph / np.sum(alph)
-
-    mean = (np.dot(D, alph_mean) + np.dot(D2, alph_mean)) / 2
-    mean /= np.linalg.norm(mean)
-
-    return mean, alph
-
-
-def compute_accuracy(model, loader, class_means):
-    features, targets_ = extract_features(model, loader)
-
-    targets = np.zeros((targets_.shape[0], 100), np.float32)
-    targets[range(len(targets_)), targets_.astype('int32')] = 1.
-    features = (features.T / (np.linalg.norm(features.T, axis=0) + EPSILON)).T
-
-    # Compute score for iCaRL
-    sqd = cdist(class_means, features, 'sqeuclidean')
-    score_icarl = (-sqd).T
-
-    return np.argsort(score_icarl, axis=1)[:, -1], targets_
+        return np.argsort(score_icarl, axis=1)[:, -1], targets_
