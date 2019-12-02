58004c01e184a3c83cd6596c8a3d6dd9cdd4e0ff
arthurdouillard <ar.douillard@gmail.com>
2019-12-02 17:51:37 +0100

    [lwf] Update model with simplier interface.

--- inclearn/models/lwf.py
+++ inclearn/models/lwf.py
@@ -1,173 +1,158 @@
+import logging
+
 import numpy as np
 import torch
-from torch import nn
 from torch.nn import functional as F
-from tqdm import trange
 
-from inclearn import factory, utils
-from inclearn.models.base import IncrementalLearner
+from inclearn.lib import factory, loops, network, utils
+from inclearn.models import IncrementalLearner
+
+logger = logging.getLogger(__name__)
 
 
 class LwF(IncrementalLearner):
-    """Implementation of Learning without Forgetting.
+    """Multi-class implementation of:
 
-    :param args: An argparse parsed arguments object.
+    * Learning without Forgetting
+      Zhizhong Li, Derek Hoiem
+      ECCV 2016
+
+    This implementation is the multi-class version introduced by iCaRL and then
+    re-used by pretty much every incremental learning papers.
     """
 
     def __init__(self, args):
-        super().__init__()
+        self._device = args["device"][0]
+        self._multiple_devices = args["device"]
 
-        self._device = args["device"]
         self._opt_name = args["optimizer"]
         self._lr = args["lr"]
+        self._lr_decay = args["lr_decay"]
         self._weight_decay = args["weight_decay"]
         self._n_epochs = args["epochs"]
-
         self._scheduling = args["scheduling"]
-        self._lr_decay = args["lr_decay"]
 
-        self._temperature = args["temperature"]
+        self._distillation_config = args["distillation_config"]
+
+        logger.info("Initializing LwM")
+
+        self._network = network.BasicNet(
+            args["convnet"],
+            convnet_kwargs=args.get("convnet_config", {}),
+            classifier_kwargs=args.get("classifier_config", {
+                "type": "fc",
+                "use_bias": True
+            }),
+            device=self._device
+        )
 
         self._n_classes = 0
-
-        self._add_n_classes(args["increment"], convnet=args["convnet"])
-
-        self.to(self._device)
-
-    def forward(self, inputs):
-        x = self._features_extractor(inputs)
-        x = self._classifier(x)
-        return x
-
-    # ----------------
-    # Public interface
-    # ----------------
+        self._old_model = None
 
     def _before_task(self, data_loader, val_loader):
-        """Computes predictions with previous model's weights & set up optimizer."""
-        if self._task == 0:
-            self._previous_preds = None
+        self._n_classes += self._task_size
+        self._network.add_classes(self._task_size)
+
+        self._optimizer = factory.get_optimizer(
+            self._network.parameters(), self._opt_name, self._lr, self._weight_decay
+        )
+        if self._scheduling is None:
+            self._scheduler = None
         else:
-            print("Computing previous predictions...")
-            self._previous_preds = self._compute_predictions(data_loader)
-            self._add_n_classes(self._task_size)
+            self._scheduler = torch.optim.lr_scheduler.MultiStepLR(
+                self._optimizer, self._scheduling, gamma=self._lr_decay
+            )
 
-        self._optimizer = factory.get_optimizer(self.parameters(), self._opt_name, self._lr,
-                                                self._weight_decay)
+    def _train_task(self, train_loader, val_loader):
+        loops.single_loop(
+            train_loader,
+            val_loader,
+            self._multiple_devices,
+            self._network,
+            self._n_epochs,
+            self._optimizer,
+            scheduler=self._scheduler,
+            train_function=self._forward_loss,
+            eval_function=self._accuracy,
+            task=self._task,
+            n_tasks=self._n_tasks
+        )
 
-        self._scheduler = torch.optim.lr_scheduler.MultiStepLR(self._optimizer,
-                                                               self._scheduling,
-                                                               gamma=self._lr_decay)
+    def _after_task(self, inc_dataset):
+        self._old_model = self._network.copy().freeze().eval().to(self._device)
+        self._network.on_task_end()
 
-    def _train_task(self, train_loader, val_loader=None):
-        val_acc = 0.
+    def _eval_task(self, loader):
+        ypred, ytrue = [], []
 
-        prog_bar = trange(self._n_epochs, desc="Losses.")
-        for epoch in prog_bar:
-            avg_clf_loss, avg_distil_loss = 0., 0.
-            c = 0
+        for input_dict in loader:
+            with torch.no_grad():
+                logits = self._network(input_dict["inputs"].to(self._device))["logits"]
 
-            self._scheduler.step()
+            ytrue.append(input_dict["targets"].numpy())
+            ypred.append(torch.softmax(logits, dim=1).cpu().numpy())
 
-            for i, ((_, idxes), inputs, targets) in enumerate(train_loader, start=1):
-                self._optimizer.zero_grad()
+        ytrue = np.concatenate(ytrue)
+        ypred = np.concatenate(ypred)
 
-                c += len(idxes)
-                inputs, targets = inputs.to(self._device), targets.to(self._device)
-                idxes = idxes.to(self._device)
-                logits = self.forward(inputs)
+        return ypred, ytrue
 
-                clf_loss, distil_loss = self._compute_loss(
-                    logits,
-                    targets,
-                    idxes,
-                )
+    def _accuracy(self, loader):
+        ypred, ytrue = self._eval_task(loader)
+        ypred = ypred.argmax(dim=1)
 
-                if not utils._check_loss(clf_loss) or not utils._check_loss(distil_loss):
-                    import pdb
-                    pdb.set_trace()
+        return 100 * round(np.mean(ypred == ytrue), 3)
 
-                loss = clf_loss + distil_loss
+    def _forward_loss(self, training_network, inputs, targets, memory_flags, metrics):
+        inputs, targets = inputs.to(self._device), targets.to(self._device)
+        onehot_targets = utils.to_onehot(targets, self._n_classes).to(self._device)
 
-                loss.backward()
-                self._optimizer.step()
+        outputs = training_network(inputs)
 
-                avg_clf_loss += clf_loss.item()
-                avg_distil_loss += distil_loss.item()
+        loss = self._compute_loss(inputs, outputs, targets, onehot_targets, memory_flags, metrics)
 
-                if i % 10 == 0:
-                    prog_bar.set_description("Clf loss: {}; Distill loss: {}; Val acc: {}".format(
-                        round(clf_loss.item(), 3), round(distil_loss.item(), 3), round(val_acc, 3)))
+        if not utils.check_loss(loss):
+            raise ValueError("Loss became invalid ({}).".format(loss))
 
-                if val_loader is not None and i % 100 == 0:
-                    val_acc = self._accuracy(self._classifier(val_loader))
+        metrics["loss"] += loss.item()
 
-            prog_bar.set_description("Clf loss: {}; Distill loss: {}; Val acc: {}".format(
-                round(clf_loss.item(), 3), round(distil_loss.item(), 3), round(val_acc, 3)))
+        return loss
 
-    def _eval_task(self, data_loader):
-        return self._classify(data_loader)
+    def _compute_loss(self, inputs, outputs, targets, onehot_targets, memory_flags, metrics):
+        logits = outputs["logits"]
 
-    # -----------------
-    # Private interface
-    # -----------------
-
-    def _add_n_classes(self, n, convnet=None):
-        if self._n_classes == 0:
-            self._features_extractor = factory.get_resnet(convnet, nf=64, zero_init_residual=True)
-            self._classifier = nn.Linear(self._features_extractor.out_dim, n, bias=False)
-            torch.nn.init.kaiming_normal_(self._classifier.weight)
+        if self._old_model is None:
+            # Classification loss
+            loss = F.cross_entropy(logits, targets)
+            metrics["clf"] += loss.item()
         else:
-            weights = self._classifier.weight.data
-            self._classifier = nn.Linear(self._features_extractor.out_dim,
-                                         self._n_classes + n,
-                                         bias=False).to(self._device)
-            torch.nn.init.kaiming_normal_(self._classifier.weight)
-            self._classifier.weight.data[:self._n_classes] = weights
+            self._old_model.zero_grad()
+            old_outputs = self._old_model(inputs)
+            old_logits = old_outputs["logits"]
 
-        self._n_classes += n
+            proba = F.softmax(logits, dim=1)
+            old_proba = F.softmax(old_logits, dim=1)
 
-    def _compute_predictions(self, data_loader):
-        logits = torch.zeros(self._n_train_data, self._n_classes, device=self._device)
+            # Classification loss
+            loss = F.binary_cross_entropy(
+                proba[..., -self._task_size:], onehot_targets[..., -self._task_size:]
+            )
 
-        for idxes, inputs, _ in data_loader:
-            inputs = inputs.to(self._device)
-            idxes = idxes[1].to(self._device)
+            # Distillation on probabilities
+            modified_proba = torch.pow(
+                proba[..., :-self._task_size], self._distillation_config["temperature"]
+            )
+            modified_old_proba = torch.pow(
+                old_proba[..., :-self._task_size], self._distillation_config["temperature"]
+            )
 
-            logits[idxes] = self.forward(inputs).detach() ** (1 / self._temperature)
+            modified_proba = modified_proba / modified_proba.sum(-1)
+            modified_old_proba = modified_old_proba / modified_old_proba.sum(-1)
 
-        return logits
+            distill_loss = self._distillation_config["factor"] * F.binary_cross_entropy_with_logits(
+                modified_proba, modified_old_proba
+            )
+            metrics["dis"] += distill_loss.item()
+            loss += distill_loss
 
-    def _compute_loss(self, logits, targets, idxes):
-        if self._task == 0:
-            clf_loss = F.cross_entropy(logits, targets)
-            distil_loss = torch.zeros(1, device=self._device)
-        else:
-            for i in range(self._new_task_index, self._n_classes):
-                targets[targets == i] = -1
-
-            clf_loss = F.cross_entropy(logits[..., self._new_task_index:], targets, ignore_index=-1)
-
-            distil_loss = F.binary_cross_entropy(
-                F.softmax(logits[..., :self._new_task_index], dim=1), self._previous_preds[idxes])
-
-        return clf_loss, distil_loss
-
-    def _classify(self, data_loader):
-        ypred = []
-        ytrue = []
-
-        for _, inputs, targets in data_loader:
-            inputs = inputs.to(self._device)
-
-            probs = self.forward(inputs).detach()
-            preds = F.softmax(probs, dim=1).argmax(dim=1).cpu().numpy().tolist()
-
-            ypred.extend(preds)
-            ytrue.extend(targets)
-
-        return np.array(ypred), np.array(ytrue)
-
-    @staticmethod
-    def _accuracy(self, ypred, ytrue):
-        return (ypred == ytrue).sum() / len(ytrue)
+        return loss
