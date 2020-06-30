8baea27a634fc84cb6cd8c3063877185e116c484
arthurdouillard <ar.douillard@gmail.com>
2020-06-30 17:16:51 +0200

    Simplify code for UCIR's ranking loss.

--- inclearn/lib/losses/base.py
+++ inclearn/lib/losses/base.py
@@ -105,49 +105,32 @@ def embeddings_similarity(features_a, features_b):
 
 
 def ucir_ranking(logits, targets, n_classes, task_size, nb_negatives=2, margin=0.2):
-    return github_ucir_ranking_mr(logits, targets, n_classes, task_size, nb_negatives, margin)
-    # Ranking loss maximizing the inter-class separation between old & new:
+    """Hinge loss from UCIR.
 
-    # 1. Fetching from the batch only samples from the batch that belongs
-    #    to old classes:
-    old_indexes = targets.lt(n_classes - 1)
-    old_logits = logits[old_indexes]
-    old_targets = targets[old_indexes]
+    Taken from: https://github.com/hshustc/CVPR19_Incremental_Learning
 
-    # 2. Getting positive values, aka ground-truth's logit predictions:
-    old_values = old_logits[torch.arange(len(old_logits)), old_targets]
-    old_values = old_values.repeat(nb_negatives, 1).t().contiguous().view(-1)
-
-    # 3. Getting top-k negative values:
-    nb_old_classes = n_classes - task_size
-    negative_indexes = old_logits[..., nb_old_classes:].argsort(dim=1, descending=True)[
-        ..., :nb_negatives] + nb_old_classes
-    new_values = old_logits[torch.arange(len(old_logits)).view(-1, 1), negative_indexes].view(-1)
-
-    return F.margin_ranking_loss(
-        old_values, new_values, -torch.ones(len(old_values)).to(logits.device), margin=margin
-    )
-
-
-def github_ucir_ranking_mr(logits, targets, n_classes, task_size, nb_negatives=2, margin=0.2):
+    # References:
+        * Learning a Unified Classifier Incrementally via Rebalancing
+          Hou et al.
+          CVPR 2019
+    """
     gt_index = torch.zeros(logits.size()).to(logits.device)
     gt_index = gt_index.scatter(1, targets.view(-1, 1), 1).ge(0.5)
     gt_scores = logits.masked_select(gt_index)
-    #get top-K scores on novel classes
+    # get top-K scores on novel classes
     num_old_classes = logits.shape[1] - task_size
     max_novel_scores = logits[:, num_old_classes:].topk(nb_negatives, dim=1)[0]
-    #the index of hard samples, i.e., samples of old classes
+    # the index of hard samples, i.e., samples of old classes
     hard_index = targets.lt(num_old_classes)
     hard_num = torch.nonzero(hard_index).size(0)
-    #print("hard examples size: ", hard_num)
+
     if hard_num > 0:
         gt_scores = gt_scores[hard_index].view(-1, 1).repeat(1, nb_negatives)
         max_novel_scores = max_novel_scores[hard_index]
         assert (gt_scores.size() == max_novel_scores.size())
         assert (gt_scores.size(0) == hard_num)
-        #print("hard example gt scores: ", gt_scores.size(), gt_scores)
-        #print("hard example max novel scores: ", max_novel_scores.size(), max_novel_scores)
         loss = nn.MarginRankingLoss(margin=margin)(gt_scores.view(-1, 1), \
             max_novel_scores.view(-1, 1), torch.ones(hard_num*nb_negatives).to(logits.device))
         return loss
+
     return torch.tensor(0).float()
