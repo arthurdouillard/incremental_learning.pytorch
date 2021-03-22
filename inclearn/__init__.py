a401a6609fc61c74698739cf937c0ece1c10913f
billpsomas <psomasbill@gmail.com>
2021-03-22 18:09:43 +0200

    minor change

--- inclearn/__init__.py
+++ inclearn/__init__.py
@@ -1,5 +1,3 @@
-# AGG backend is for writing to file, not for rendering in a window.
-# For more info, check this: https://matplotlib.org/2.0.2/faq/usage_faq.html
 import matplotlib; matplotlib.use('Agg')
 
 from inclearn import parser, train
